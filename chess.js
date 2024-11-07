0c9a5d672c53df09ef1b886f30ad03480790b9bd
Andrey <158809049+AndreyDimitrov3@users.noreply.github.com>
2024-11-07 23:35:34 +0200

    Update chess.js

--- chess.js
+++ chess.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let sectionBoardInnerHtml = "";
     let lastBackgroundRow = "darkBackground";
     let selectedPiece = null;
+    let moves = 0;
+    const pawnStartingPosition = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
     function resetValidation() {
         validation = [];
@@ -34,33 +36,33 @@ document.addEventListener("DOMContentLoaded", function() {
         const whiteRook = document.querySelectorAll(`[data-square="1"], [data-square="8"]`);
         const whiteKnight = document.querySelectorAll(`[data-square="2"], [data-square="7"]`);
         const whiteBishop = document.querySelectorAll(`[data-square="3"], [data-square="6"]`);
-        const whiteQueen = document.querySelector(`[data-square="4"]`);
-        const whiteKing = document.querySelector(`[data-square="5"]`);
+        const whiteKing = document.querySelector(`[data-square="4"]`);
+        const whiteQueen = document.querySelector(`[data-square="5"]`);
 
         for(let i = 9; i <= 16; i++) {
-            document.querySelector(`[data-square="${i}"]`).innerHTML = `<img src="assets/whitePawn.svg" alt="Pawn" data-piece="pawn">`;
+            document.querySelector(`[data-square="${i}"]`).innerHTML = `<img src="assets/whitePawn.svg" alt="Pawn" data-piece="pawn" data-color="white" data-startingPosition="${pawnStartingPosition[i - 9]}2">`;
         }
 
         whiteRook.forEach(rook => {
             rook.innerHTML = `
-                <img src="assets/whiteRook.svg" alt="Rook" data-piece="rook">
+                <img src="assets/whiteRook.svg" alt="Rook" data-piece="rook" data-color="white">
             `;
         });
 
         whiteKnight.forEach(knight => {
             knight.innerHTML = `
-                <img src="assets/whiteKnight.svg" alt="Knight" data-piece="knight">
+                <img src="assets/whiteKnight.svg" alt="Knight" data-piece="knight" data-color="white">
             `;
         });
 
         whiteBishop.forEach(bishop => {
             bishop.innerHTML = `
-                <img src="assets/whiteBishop.svg" alt="Bishop" data-piece="bishop">
+                <img src="assets/whiteBishop.svg" alt="Bishop" data-piece="bishop" data-color="white">
             `;
         });
 
-        whiteQueen.innerHTML = `<img src="assets/whiteQueen.svg" alt="Queen" data-piece="queen">`;
-        whiteKing.innerHTML = `<img src="assets/whiteKing.svg" alt="King" data-piece="king">`;
+        whiteKing.innerHTML = `<img src="assets/whiteKing.svg" alt="King" data-piece="king" data-color="white">`;
+        whiteQueen.innerHTML = `<img src="assets/whiteQueen.svg" alt="Queen" data-piece="queen" data-color="white">`;
     }
 
     function resetBlackPieces() {
@@ -71,29 +73,29 @@ document.addEventListener("DOMContentLoaded", function() {
         const blackKing = document.querySelector(`[data-square="60"]`);
 
         for(let i = 49; i <= 56; i++) {
-            document.querySelector(`[data-square="${i}"]`).innerHTML = `<img src="assets/blackPawn.svg" alt="Pawn" data-piece="pawn">`;
+            document.querySelector(`[data-square="${i}"]`).innerHTML = `<img src="assets/blackPawn.svg" alt="Pawn" data-piece="pawn" data-color="black" data-startingposition="${pawnStartingPosition[i - 49]}7">`;
         }
 
         blackRook.forEach(rook => {
             rook.innerHTML = `
-                <img src="assets/blackRook.svg" alt="Rook" data-piece="rook">
+                <img src="assets/blackRook.svg" alt="Rook" data-piece="rook" data-color="black">
             `;
         });
 
         blackKnight.forEach(knight => {
             knight.innerHTML = `
-                <img src="assets/blackKnight.svg" alt="Knight" data-piece="knight">
+                <img src="assets/blackKnight.svg" alt="Knight" data-piece="knight" data-color="black">
             `;
         });
 
         blackBishop.forEach(bishop => {
             bishop.innerHTML = `
-                <img src="assets/blackBishop.svg" alt="Bishop" data-piece="bishop">
+                <img src="assets/blackBishop.svg" alt="Bishop" data-piece="bishop" data-color="black">
             `;
         });
 
-        blackQueen.innerHTML = `<img src="assets/blackQueen.svg" alt="Queen" data-piece="queen">`;
-        blackKing.innerHTML = `<img src="assets/blackKing.svg" alt="King" data-piece="king">`;
+        blackQueen.innerHTML = `<img src="assets/blackQueen.svg" alt="Queen" data-piece="queen" data-color="black">`;
+        blackKing.innerHTML = `<img src="assets/blackKing.svg" alt="King" data-piece="king" data-color="black">`;
     }
     resetWhitePieces();
     resetBlackPieces();
@@ -104,6 +106,11 @@ document.addEventListener("DOMContentLoaded", function() {
                 selectedPiece.classList.remove("selected");
             }
 
+            let player = moves % 2 === 0 ? "white" : "black";
+            if(event.target.dataset.color !== player) {
+                return;
+            }
+
             selectedPiece = event.target;
             selectedPiece.classList.add("selected");
 
@@ -111,29 +118,133 @@ document.addEventListener("DOMContentLoaded", function() {
 
             document.querySelectorAll("[data-square]").forEach(square => {
                 square.addEventListener("click", function squareClick() {
+                    let isMoveCorrect = false;
                     try {
                         switch(selectedPiece.dataset.piece) {
                             case "pawn":
-                                pawnMoveChecker()
+                                isMoveCorrect = pawnMoveChecker(selectedPiece, square, player);
+                                break;
+                            case "rook":
+                                isMoveCorrect = rookMoveChecker(selectedPiece, square, player);
+                                break;
+                            case "knight":
+                                isMoveCorrect = knightMoveChecker(selectedPiece, square, player);
+                                break;
+                            case "bishop":
+                                isMoveCorrect = bishopMoveChecker(selectedPiece, square, player);
+                                break;
+                            case "queen":
+                                isMoveCorrect = queenMoveChecker(selectedPiece, square, player);
+                                break;
+                            case "king":
+                                isMoveCorrect = kingMoveChecker(selectedPiece, square, player);
                                 break;
                         }
+
+                        if(isMoveCorrect) {
+                            square.appendChild(selectedPiece);
+                            selectedPiece.classList.remove("selected");
+                            selectedPiece = null;
+                            moves++;
+                        }
+
+                        document.querySelectorAll("[data-square]").forEach(square => {
+                            square.removeEventListener("click", squareClick);
+                        });
                     } catch(error) {
                         return;
                     }
-
-                    square.appendChild(selectedPiece);
-                    selectedPiece.classList.remove("selected");
-                    selectedPiece = null; 
-
-                    document.querySelectorAll("[data-square]").forEach(square => {
-                        square.removeEventListener("click", squareClick);
-                    });
                 });
             });
         });
     });
 
-    function pawnMoveChecker() {
+    function pawnMoveChecker(selectedPiece, movedSquare, player) {
+        let playerPawn = player === "white" ? 1 : -1;
+        const previousSquare = selectedPiece.closest(".block");
+        const previousSquareRow = parseInt(previousSquare.dataset.row);
+        const previousSquareColumn = parseInt(previousSquare.dataset.column);
+        const movedSquareRow = parseInt(movedSquare.dataset.row);
+        const movedSquareColumn = parseInt(movedSquare.dataset.column);
+        const checkLeftDiagonal = document.querySelector(`[data-row="${previousSquareRow + playerPawn}"][data-column="${previousSquareColumn + 1}"]`);
+        const checkRightDiagonal = document.querySelector(`[data-row="${previousSquareRow + playerPawn}"][data-column="${previousSquareColumn - 1}"]`);
 
+        console.log(selectedPiece.closest(".block"))
+        if ((player === "white" && previousSquareRow === 2) || (player === "black" && previousSquareRow === 7)) {
+            playerPawn = player === "white" ? 2 : -2;
+            if (selectedPiece.dataset.color === "white" && 
+                movedSquareRow <= previousSquareRow + playerPawn && 
+                previousSquareColumn === movedSquareColumn) {
+                return columnChecker(previousSquareRow, movedSquareRow, previousSquareColumn, player);
+            } else if(selectedPiece.dataset.color === "black" && 
+                movedSquareRow >= previousSquareRow + playerPawn && 
+                previousSquareColumn === movedSquareColumn) {
+                    return columnChecker(previousSquareRow, movedSquareRow, previousSquareColumn, player);
+                }
+        } else {
+            if (selectedPiece.dataset.color === player && 
+                movedSquareRow === previousSquareRow + playerPawn && 
+                previousSquareColumn === movedSquareColumn) {
+                    return columnChecker(previousSquareRow, movedSquareRow, previousSquareColumn, player);
+            }
+        }
+
+        if (selectedPiece.dataset.color === player) {
+            if (movedSquare === checkLeftDiagonal && checkLeftDiagonal && checkLeftDiagonal.firstChild && checkLeftDiagonal.firstChild.dataset.color !== player) {
+                capturePiece(checkLeftDiagonal);
+                return true;
+            }
+        
+            if (movedSquare === checkRightDiagonal && checkRightDiagonal && checkRightDiagonal.firstChild && checkRightDiagonal.firstChild.dataset.color !== player) {
+                capturePiece(checkRightDiagonal);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function rookMoveChecker(selectedPiece, movedSquare, player) {
+        return true;
+    }
+
+    function knightMoveChecker(selectedPiece, movedSquare, player) {
+        return true;
+    }
+
+    function bishopMoveChecker(selectedPiece, movedSquare, player) {
+        return true;
+    }
+
+    function queenMoveChecker(selectedPiece, movedSquare, player) {
+        return true;
+    }
+
+    function kingMoveChecker(selectedPiece, movedSquare, player) {
+        return true;
+    }
+
+    function columnChecker(previousSquareRow, movedSquareRow, previousSquareColumn, player) {
+        let step = player === "white" ? 1 : -1;
+
+        console.log(previousSquareRow, movedSquareRow, previousSquareColumn, player)
+        for (let i = previousSquareRow + step; player === "white" ? i <= movedSquareRow : i >= movedSquareRow; i += step) {
+            const targetSquare = document.querySelector(`[data-row="${i}"][data-column="${previousSquareColumn}"]`);
+            if(targetSquare && targetSquare.firstChild) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function capturePiece(takenPiece) {
+        checkExposeKing();
+        const parentElement = takenPiece.closest(".block");
+        const takenPieceImg = takenPiece.firstChild;
+
+        parentElement.removeChild(takenPieceImg)
+    }
+
+    function checkExposeKing() {
+        return true;
     }
 })
